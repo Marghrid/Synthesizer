9ac0919dd4c2dd8aad57c30f6f5a0a1f780df7e2
Daniel Ramos <daniel.r.ramos@tecnico.ulisboa.pt>
2019-12-10 11:31:55 +0000

    improvements for date detection

--- benchmarks/isDate.R
+++ benchmarks/isDate.R
@@ -1,10 +1,22 @@
 first <- unname(unlist(sapply(df[1,], as.character)))
 
-dateFormats = c("%d/%m/%Y", "%d-%m-%Y", "%Y/%m/%d", "%Y-%m-%d", "%m/%d/%Y")
+dateFormats = c("%d/%m/%Y", "%d-%m-%Y", "%Y/%m/%d", "%Y-%m-%d", "%m/%d/%Y", "%m-%d-%Y")
+lubriFormat = c("dmy", "dmy", "ymd", "ymd", "mdy", "mdy")
 
 isDate <- function(x) 
             !(is.na(as.Date(as.character(x),
-                            format=dateFormats)))
+                            format=dateFormats, tz = "UTC")))
 
-isDate(first[1])
-isDate(first[2])
+
+out <- c()
+formats <- list()
+for (i in 1:length(first)) {
+    truth_value <- isDate(first[i])
+    if (!all(!truth_value)) {
+        out <- append(out, i)
+        formats[[i]] <- c(lubriFormat[truth_value])
+    }
+}
+
+out
+formats
