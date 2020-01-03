9dbbeef72b4405cacd505c3d8cf1e40939dd2d0e
Daniel Ramos <daniel.r.ramos@tecnico.ulisboa.pt>
2020-01-03 15:50:46 +0000

    Script to run tests

--- run_nn.py
+++ run_nn.py
@@ -1,16 +1,69 @@
 import subprocess
 
 template = 'python3 NN/predict.py -i PATH -m MAX'
+benchmarks = {'Q1-flight_delays_out.png': [45, 12],
+              'Q2-consumer_complaints_out.png': [65800, 10],
+              'Q3-consumer_complaints_out.png': [221000, 6],
+              'Q4-health-facilities-gh_out.png': [735, 10],
+              'Q6-output-avocado.png': [2, 4],
+              'Q7-output-avocado.png': [6200000000, 4],
+              'Q8-output-avocado.png': [1330000000, 3],
+              'Q9-output-titanic.png': [270, 2],
+              'Q10-output-titanic.png': [445, 3],
+              'Q11-output-titanic.png': [285, 5],
+              'Q12-output-bikes.png': [815, 5],
+              'Q13-output-bikes.png': [40, 5],
+              'Q14-airline-sentiment_out.png': [3525, 10],
+              'Q15-output-bank.png': [1330, 2],
+              'Q16-output-bank.png': [4605, 6],
+              'Q17-output-work.png': [250, 2],
+              'Q18-output-work.png': [1, 10],
+              'Q19-output-work.png': [6810, 6],
+              'Q20-instacart_out.png': [1, 2],
+              'Q21-crimes-in-boston_out.png': [40500, 10],
+              'Q22-immigrants_by_nationality_out.png': [6695, 10],
+              'Q24-population-barcelona_out.png': [70, 2],
+              'Q26-youtube-traffic_out.png': [115, 10],
+              'Q27-youtube-traffic_out.png': [5140, 10],
+              'Q28-youtube-traffic_out.png': [15100, 10],
+              'Q29-youtube-traffic_out.png': [535, 10],
+              'Q30-output-wine.png': [90 ,5],
+              'Q31-output-wine.png': [3180, 5],
+              'Q32-output-pokemon.png': [100, 2],
+              'Q33-output-pokemon.png': [195, 6],
+              'Q34-output-pokemon.png': [145, 5],
+              'Q35-airbnb-nyc_out.png': [1, 5],
+              'Q36-airbnb-nyc_out.png': [1, 3],
+              'Q37-airbnb-nyc_out.png': [175, 5],
+              'Q38-airbnb-nyc_out.png': [360, 11],
+              'Q39-gun-violence_out.png': [610, 6],
+              'Q40-gun-violence_out.png': [3, 14],
+              'Q41-gun-violence_out.png': [355, 7],
+              'Q42-employee_out.png': [615, 5],
+              'Q43-employee_out.png': [3170, 2],
+              'Q44-members_out.png': [735, 14],
+              'Q45-members_out.png': [430, 7],
+              'Q46-members_out.png': [1020, 10],
+              'Q47-members_out.png': [1590, 3],
+              'Q48-videogame_out.png': [9725, 5],
+              'Q49-videogame_out.png': [11200, 10],
+              'Q50-videogame_out.png': [80, 10],
+              'Q51-aircraft_out.png': [15, 10],
+              'Q52-appstore_out.png': [20, 7],
+              'Q53-appstore_out.png': [10800, 7],
+              'Q54-appstore_out.png': [11500, 4],
+              'Q55-appstore_out.png': [20, 3]
+              }
 
-args = template.replace('PATH', 'benchmarks/bar_charts/Q3-consumer_complaints_out.png')
-args = args.replace('MAX', str(221000))
-output = subprocess.check_output(args.split())
-output = output.decode('utf-8')
-n_bars, values = eval(output)
-
-bars = [('bar{}'.format(i), values[i]) for i in range(n_bars)]
-with open('NN/' + 'Q3-consumer_complaints_out.png'.replace('.png', '-nn.csv'), "a+") as f:
-    print('{},{}'.format('col0','col1'), file=f)
-    for bar in bars:
-        print('{},{}'.format(bar[0],bar[1]), file=f)
+for name in benchmarks:
+    args = template.replace('PATH', 'benchmarks/bar_charts/' + name)
+    args = args.replace('MAX', str(benchmarks[name][0]))
+    output = subprocess.check_output(args.split())
+    output = output.decode('utf-8')
+    n_bars, values = eval(output)
+    bars = [('bar{}'.format(i), values[i]) for i in range(n_bars)]
+    with open('NN/' + name.replace('.png', '-nn.csv'), "a+") as f:
+        print('{},{}'.format('col0', 'col1'), file=f)
+        for bar in bars:
+            print('{},{}'.format(bar[0], bar[1]), file=f)
 
---
+++ run_synth.py
@@ -0,0 +1,84 @@
+import subprocess
+from os import listdir
+from os.path import isfile, join
+
+MAX_LOC = 6
+outputs = ['benchmarks/real_output_tables/' + f for f in listdir('benchmarks/real_output_tables/')]
+
+template = 'python3 synthesizer.py -i0 INPUT -o OUTPUT -l LINES -d DEPTH -t dsl.tyrell -m MAX'
+benchmarks = {'benchmarks/input_tables/Q2-consumer_complaints.csv': [65800, 3, 4],
+              'benchmarks/input_tables/Q3-consumer_complaints.csv': [221000, 3, 4],
+              'benchmarks/input_tables/Q4-health-facilities-gh.csv': [735, 3, 4],
+              'benchmarks/input_tables/Q6-input-avocado.csv': [2, 3, 4],
+              'benchmarks/input_tables/Q7-input-avocado.csv': [6200000000, 2, 3],
+              'benchmarks/preprocessed_input_tables/Q8-input-avocado.csv': [1330000000, 3, 4],
+              'benchmarks/preprocessed_input_tables/Q9-input-titanic.csv': [270, 2, 3],
+              'benchmarks/preprocessed_input_tables/Q10-input-titanic.csv': [445, 2, 3],
+              'benchmarks/preprocessed_input_tables/Q11-input-titanic.csv': [285, 2, 3],
+              'benchmarks/input_tables/Q12-input-bikes.csv': [815, 2, 3],
+              'benchmarks/input_tables/Q13-input-bikes.csv': [40, 2, 3],
+              'benchmarks/input_tables/Q14-airline-sentiment.csv': [3525, 3, 4],
+              'benchmarks/preprocessed_input_tables/Q15-input-bank.csv': [1330, 2, 3],
+              'benchmarks/preprocessed_input_tables/Q16-input-bank.csv': [4605, 2, 3],
+              'benchmarks/input_tables/Q17-input-work.csv': [250, 2, 3],
+              'benchmarks/preprocessed_input_tables/Q19-input-work.csv': [6180, 2, 3],
+              'benchmarks/input_tables/Q20-instacart.csv': [1, 3, 4],
+              'benchmarks/input_tables/Q21-crimes-in-boston.csv': [40500, 3, 4],
+              'benchmarks/preprocessed_input_tables/Q22-immigrants_by_nationality.csv': [6695, 3, 4],
+              'benchmarks/preprocessed_input_tables/Q23-deaths-barcelona.csv': [1, 3, 4],
+              'benchmarks/input_tables/Q24-population-barcelona.csv': [70, 3, 4],
+              'benchmarks/input_tables/Q26-youtube-traffic.csv': [115, 3, 4],
+              'benchmarks/input_tables/Q27-youtube-traffic.csv': [5140, 3, 4],
+              'benchmarks/input_tables/Q28-youtube-traffic.csv': [15100, 4, 5],
+              'benchmarks/input_tables/Q29-youtube-traffic_out.png': [535, 3, 4],
+              'benchmarks/preprocessed_input_tables/Q30-wine-input.csv': [90, 4, 5],
+              'benchmarks/input_tables/Q33-input-pokemon.csv': [195, 2, 3],
+              'benchmarks/input_tables/Q34-input-pokemon.csv': [145, 3, 4],
+              'benchmarks/input_tables/Q35-airbnb-nyc.csv': [1, 3, 4],
+              'benchmarks/input_tables/Q36-airbnb-nyc.csv': [1, 3, 4],
+              'benchmarks/input_tables/Q37-airbnb-nyc.csv': [175, 2, 3],
+              'benchmarks/input_tables/Q38-airbnb-nyc.csv': [360, 3, 4],
+              'benchmarks/input_tables/Q39-gun-violence.csv': [610, 3, 4],
+              'benchmarks/input_tables/Q41-gun-violence.csv': [355, 3, 4],
+              'benchmarks/input_tables/Q42-employee.csv': [615, 2, 3],
+              'benchmarks/input_tables/Q43-employee.csv': [3170, 2, 3],
+              'benchmarks/input_tables/Q44-employee.csv': [735, 3, 4],
+              'benchmarks/input_tables/Q45-members.csv': [430, 3, 4],
+              'benchmarks/input_tables/Q46-members.csv': [1020, 2, 3],
+              'benchmarks/input_tables/Q47-members.csv': [1590, 2, 3],
+              'benchmarks/input_tables/Q48-videogame.csv': [9725, 3, 4],
+              'benchmarks/input_tables/Q49-videogame.csv': [11200, 2, 3],
+              'benchmarks/input_tables/Q50-videogame.csv': [80, 3, 4],
+              'benchmarks/input_tables/Q51-aircraft.csv': [15, 4, 5],
+              'benchmarks/input_tables/Q52-appstore.csv': [20, 3, 4],
+              'benchmarks/input_tables/Q53-appstore.csv': [10800, 2, 3],
+              'benchmarks/input_tables/Q54-appstore.csv': [11500, 2, 3],
+              'benchmarks/input_tables/Q55-appstore.csv': [20, 3, 4]
+              }
+
+for name in benchmarks:
+    for i in range(2, MAX_LOC):
+        args = template.replace('INPUT', name)
+        args = args.replace('MAX', str(benchmarks[name][0]))
+        args = args.replace('LINES', str(i))
+        args = args.replace('DEPTH', str(i+1))
+
+        string = name[name.find('input_tables/Q') + len('input_tables/Q'):]
+        first, second = string[0], string[1]
+        query = ''
+        if second.isdigit():
+            query = 'Q' + first + second + '-'
+        else:
+            query = 'Q' + first + '-'
+
+        output = next(filter(lambda x: query in x, outputs))
+        args = args.replace('OUTPUT', output)
+        output = subprocess.check_output(args.split())
+        output = output.decode('utf-8')
+        n_bars, values = eval(output)
+        bars = [('bar{}'.format(i), values[i]) for i in range(n_bars)]
+        with open('NN/' + name.replace('.png', '-nn.csv'), "a+") as f:
+            print('{},{}'.format('col0', 'col1'), file=f)
+            for bar in bars:
+                print('{},{}'.format(bar[0], bar[1]), file=f)
+
